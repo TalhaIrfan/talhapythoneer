9d29ade392b8bea499ba962e850bf7638ca5d421
Talha <talha.ifn@gmail.com>
2022-06-19 16:25:03 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,17 +1,1 @@
-@talhapythoneer
-
-![](https://github.com/talhapythoneer/github-stats/blob/master/generated/overview.svg)
-![](https://github.com/talhapythoneer/github-stats/blob/master/generated/languages.svg)
-
-Hi, it's TalhaPythoneer. I have been working for over 3 years as a Web Scraper and Web Developer. I am an expert in Web Scraping, Data Extraction, building ETL pipelines, and developing responsive websites and Web Applications.
-
-I can scrape any website e.g Dynamic (JavaScript / Ajax-based) websites, Login Required Sites, and IP Blocking Sites using Python. I can provide Scrapped data in CSV, XLSX (Excel or Google sheets), JSON, MongoDB, or any other desired database.
-
-My development skillset revolves around Django and React JS. I have built different web projects including management systems, messengers, ML apps, and one-page portfolios or company websites.
-
-I have a strong grip on:
-- Python, Scrapy, Splash, Selenium, Requests, BeaitufulSoup, Pandas and Numpy
-- HTML, CSS, Bootstrap, React JS and Django
-
-I am also skilled with:
-- C++, JAVA, JavaScript, Dart, HTML/CSS
+# talhapythoneer
---
+++ js/index.js
@@ -0,0 +1,158 @@
+function initTooltip() {
+	const tooltips = Array.from(document.querySelectorAll('[data-tooltip-container]'));
+
+	tooltips.map(tooltip => {
+		tooltip.addEventListener('mouseover', handleMouseOver);
+	})
+
+	function handleMouseOver() {
+		const tooltipbox = createTooltipBox(this);
+
+		handleMouseMove.tooltipbox = tooltipbox;
+		this.addEventListener('mousemove', handleMouseMove);
+
+		handleMouseLeave.tooltipbox = tooltipbox;
+		handleMouseLeave.element = this;
+		this.addEventListener('mouseleave', handleMouseLeave);
+	}
+
+	const handleMouseLeave = {
+		handleEvent() {
+			this.tooltipbox.remove();
+			this.element.removeEventListener('mousemove', handleMouseMove);
+			this.element.removeEventListener('mouseleave', handleMouseLeave);
+		}
+	}
+
+	const handleMouseMove = {
+		handleEvent(e) {
+			this.tooltipbox.style.top = e.clientY + 25 + 'px';
+			this.tooltipbox.style.left = e.clientX + 13 +'px';
+		}
+	}
+
+	function createTooltipBox(el) {
+		let tooltip = document.createElement('div');
+		tooltip.innerText = el.getAttribute('data-tooltip-label');
+		tooltip.classList.add('tooltip');
+
+		document.body.appendChild(tooltip);
+		
+		return tooltip;
+	}
+}
+
+initTooltip();
+
+
+
+// JS for testimonials slidesow
+// vars
+'use strict'
+var	testim = document.getElementById("testim"),
+		testimDots = Array.prototype.slice.call(document.getElementById("testim-dots").children),
+    testimContent = Array.prototype.slice.call(document.getElementById("testim-content").children),
+    testimLeftArrow = document.getElementById("left-arrow"),
+    testimRightArrow = document.getElementById("right-arrow"),
+    testimSpeed = 4500,
+    currentSlide = 0,
+    currentActive = 0,
+    testimTimer,
+		touchStartPos,
+		touchEndPos,
+		touchPosDiff,
+		ignoreTouch = 30;
+;
+
+window.onload = function() {
+
+    // Testim Script
+    function playSlide(slide) {
+        for (var k = 0; k < testimDots.length; k++) {
+            testimContent[k].classList.remove("active");
+            testimContent[k].classList.remove("inactive");
+            testimDots[k].classList.remove("active");
+        }
+
+        if (slide < 0) {
+            slide = currentSlide = testimContent.length-1;
+        }
+
+        if (slide > testimContent.length - 1) {
+            slide = currentSlide = 0;
+        }
+
+        if (currentActive != currentSlide) {
+            testimContent[currentActive].classList.add("inactive");            
+        }
+        testimContent[slide].classList.add("active");
+        testimDots[slide].classList.add("active");
+
+        currentActive = currentSlide;
+    
+        clearTimeout(testimTimer);
+        testimTimer = setTimeout(function() {
+            playSlide(currentSlide += 1);
+        }, testimSpeed)
+    }
+
+    testimLeftArrow.addEventListener("click", function() {
+        playSlide(currentSlide -= 1);
+    })
+
+    testimRightArrow.addEventListener("click", function() {
+        playSlide(currentSlide += 1);
+    })    
+
+    for (var l = 0; l < testimDots.length; l++) {
+        testimDots[l].addEventListener("click", function() {
+            playSlide(currentSlide = testimDots.indexOf(this));
+        })
+    }
+
+    playSlide(currentSlide);
+
+    // keyboard shortcuts
+    document.addEventListener("keyup", function(e) {
+        switch (e.keyCode) {
+            case 37:
+                testimLeftArrow.click();
+                break;
+                
+            case 39:
+                testimRightArrow.click();
+                break;
+
+            case 39:
+                testimRightArrow.click();
+                break;
+
+            default:
+                break;
+        }
+    })
+		
+		testim.addEventListener("touchstart", function(e) {
+				touchStartPos = e.changedTouches[0].clientX;
+		})
+	
+		testim.addEventListener("touchend", function(e) {
+				touchEndPos = e.changedTouches[0].clientX;
+			
+				touchPosDiff = touchStartPos - touchEndPos;
+			
+				console.log(touchPosDiff);
+				console.log(touchStartPos);	
+				console.log(touchEndPos);	
+
+			
+				if (touchPosDiff > 0 + ignoreTouch) {
+						testimLeftArrow.click();
+				} else if (touchPosDiff < 0 - ignoreTouch) {
+						testimRightArrow.click();
+				} else {
+					return;
+				}
+			
+		})
+}
